790334d310be3e5c36a94aadbef9a80da8ab61ae
Ricardo <ricardobolgeg@gmail.com>
2022-10-11 13:15:40 +0200

    Teacher package created

---
+++ RA2_BasesDeDatosRelacionalesAD/src/Teacher/TeacherMenu.java
@@ -0,0 +1,35 @@
+package Teacher;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class TeacherMenu {
+	private JFrame menuFrame;
+	private void initialize() {
+		menuFrame = new JFrame();
+		menuFrame.setResizable(false);
+		menuFrame.setBounds(100, 100, 328, 300);
+		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		{
+			JButton button=new JButton();
+			button.setText("Button");
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					{
+						
+						menuFrame.dispose();
+					}
+				}
+			});
+			menuFrame.add(button);
+		}
+		
+		menuFrame.setVisible(true);
+	}
+	
+}
